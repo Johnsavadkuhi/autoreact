9dd69650f79d62c6efae014006d22c4099dd172c
john <physitime@gmail.com>
2020-03-30 06:40:40 +0430

    created

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const chokidar  = require('chokidar'); 
+
+// Initialize watcher.
+const watcher = chokidar.watch('./src', {
+    ignored: /(^|[\/\\])\../, // ignore dotfiles
+    persistent: true , 
+    ignoreInitial : true ,
+
+  });
+   
+  // Something to use when events are received.
+  const log = console.log.bind(console);
+  // Add event listeners.
+  watcher
+    .on('add', path => {
+
+    })
+    
+    .on('change', path => log(`File ${path} has been changed`))
+    .on('unlink', path => log(`File ${path} has been removed`));
+   
+  

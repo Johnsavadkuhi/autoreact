68222ec0cd3800d6d07bc197c02b5002e7bc78bc
john <physitime@gmail.com>
2020-04-08 05:42:08 +0430

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
-import './autoreact'; 
+'use strict';
+
+const chokidar = require('chokidar');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = (function autoreact() {
+
+  const watcher = chokidar.watch('./src', {
+    ignored: /index.js/,
+    persistent: true,
+    ignoreInitial: true,
+
+  });
+
+  const log = console.log.bind(console);
+
+  watcher.on('add', (p, event) => {
+
+    if (path.parse(p).name[0] !== path.parse(p).name[0].toUpperCase()) {
+      log("need to be rename : ", p);
+
+    }
+    if (path.extname(p) === ".js") {
+      
+      fs.writeFile(p, `import React from 'react' \n
+        
+     function ${path.parse(p).name[0].toUpperCase() }${ path.parse(p).name.slice(1).toLowerCase()}()
+     {
+      
+         return (<div></div>);
+
+     }
+        
+     export default ${path.parse(p).name[0].toUpperCase()}${  path.parse(p).name.slice(1).toLocaleLowerCase()} ; 
+        `, function (e) {
+        if (e) {
+          throw e ; 
+        }
+      });
+    }
+  })
+})()

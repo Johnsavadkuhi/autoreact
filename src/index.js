3a5958f785d2e5d9d54e7065070213667d97e5d0
john <physitime@gmail.com>
2020-04-08 05:03:10 +0430

    update

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
 'use strict';
 
-const chokidar  = require('chokidar'); 
-const fs = require('fs'); 
-const path = require('path'); 
+const chokidar = require('chokidar');
+const fs = require('fs');
+const path = require('path');
 
-function autoreact (){
-  
+function autoreact() {
+
   const watcher = chokidar.watch('./src', {
     ignored: /index.js/,
-    persistent: true , 
-    ignoreInitial : true ,
+    persistent: true,
+    ignoreInitial: true,
 
   });
-   
-  const log = console.log.bind(console);
-  
-  watcher.on('add', (p , event) => {
 
-      if(path.parse(p).name[0] !== path.parse(p).name[0].toUpperCase()){
-        log("need to be rename : " , p);
-        
-     }
-      if(path.extname(p)===".js"){
-        fs.writeFile(p , `import React from 'react' \n
+  const log = console.log.bind(console);
+
+  watcher.on('add', (p, event) => {
+
+    if (path.parse(p).name[0] !== path.parse(p).name[0].toUpperCase()) {
+      log("need to be rename : ", p);
+
+    }
+    if (path.extname(p) === ".js") {
+      fs.writeFile(p, `import React from 'react' \n
         
      function ${path.parse(p).name[0].toUpperCase() }${ path.parse(p).name.slice(1).toLowerCase()}(){
       return (<div></div>);
@@ -30,13 +30,17 @@ function autoreact (){
         }
         
         export default ${path.parse(p).name[0].toUpperCase()}${  path.parse(p).name.slice(1).toLocaleLowerCase()} ; 
-        ` , function(e){if (e) log(e)});
+        `, function (e) {
+        if (e) log(e)
+      });
 
-     
 
-      }
 
-    })
+    }
+
+  })
 }
 
-module.exports ={autoreact} ; 
+module.exports = {
+  autoreact
+};
